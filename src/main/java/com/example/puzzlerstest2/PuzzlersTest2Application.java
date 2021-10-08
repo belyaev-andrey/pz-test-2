3e570a5edf414f78f7d1587e338882a0acfd650e
Andrey Belyaev <belyaev.andrey@gmail.com>
2021-10-08 14:59:30 +0300

    Initial commit

---
+++ src/main/java/com/example/puzzlerstest2/PuzzlersTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.puzzlerstest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlersTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuzzlersTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/puzzlerstest2/PuzzlersTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.puzzlerstest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlersTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
